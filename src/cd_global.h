68dea620308220ab917312641a5e2b14d5ae70b8
kyushick <kyushick@gmail.com>
2014-09-22 11:09:11 -0500

    fixed compilation problem 2

--- src/cd_global.h
+++ src/cd_global.h
@@ -41,9 +41,9 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 #include <iostream>
 #include <assert.h>
 
-//#if _MPI_VER
+#if _MPI_VER
 #include <mpi.h>
-//#endif
+#endif
 
 //#include "cd_path.h"
 //#include "cd_id.h"
@@ -180,7 +180,7 @@ namespace cd {
 #else
 #define ROOT_COLOR 0 
 #define INITIAL_COLOR 0
-#define ColorT MPI_Comm
+#define ColorT int
 #endif
 
 /* 
