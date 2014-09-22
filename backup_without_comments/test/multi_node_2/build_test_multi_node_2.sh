b5335547c6c98cbad5c952f66abf303cc94b18de
kyushick <kyushick@gmail.com>
2014-09-21 21:19:51 -0500

    delete some files in backups

--- backup_without_comments/test/multi_node_2/build_test_multi_node_2.sh
+++
@@ -1,8 +0,0 @@
-#Uncomment here if you want to build from source code
-
-rm test_multi_node_2
-cd ../../src/
-make clean
-make
-cd ../test/multi_node_2
-mpic++  -std=gnu++0x -o test_multi_node_2 -I../../src ./test_multi_node_2.cc -L../../lib -Wl,-rpath ../../lib -lcds 
