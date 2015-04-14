f6e7fc44a8bedf834c81a46103c582c411767f37
kyushick <kyushick@gmail.com>
2015-04-13 22:57:08 -0500

    deleted old test cases

--- test_old/test_mpiwrapper/build_test_mpiwrapper.sh
+++
@@ -1,8 +0,0 @@
-#Uncomment here if you want to build from source code
-
-cd ../../src/
-make clean
-make MPI_VER_VAR=1 SINGLE_VER_VAR=0 DEBUG_VAR=1 LOGGING_ENABLED=0
-cd ../test/test_mpiwrapper
-make clean
-make
