9f3b3ecf45c671f24cd847111562c76a640fbdba
kyushick <kyushick@gmail.com>
2016-04-10 03:54:31 -0500

    added def for prv

---
+++ src/cd_def_preserve.h
@@ -0,0 +1,20 @@
+#ifndef _CD_DEF_PRESERVE_H
+#define _CD_DEF_PRESERVE_H
+
+#define MAX_FILE_PATH 256
+#define CD_FILEPATH_INVALID "./error_logs/"
+#define INIT_FILE_PATH "INITIAL_FILE_PATH"
+#define CD_FILEPATH_PFS "PFS/"
+#define CD_FILEPATH_HDD "HDD/"
+#define CD_FILEPATH_SSD "SSD/"
+#define CD_DEFAULT_PRV_GLOBAL_BASEPATH "./"
+#define CD_DEFAULT_PRV_LOCAL_BASEPATH "/tmp/"
+#define CD_DEFAULT_PRV_FILENAME "prv_files_%s_XXXXXX"
+#define CD_DEFAULT_FILEPATH "./prv_files_XXXXXX"
+#define CD_DEFAULT_DEBUG_OUT "./debug_logs/"
+#define CD_SHARING_DEGREE 64
+
+
+
+
+#endif
