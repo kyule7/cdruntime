89812abb7d6f6ff7f7814c1056d12c413eeb25bc
Kyushick Lee <kyushick@login2.stampede2.tacc.utexas.edu>
2018-03-22 19:42:32 -0500

    updated launch scripts

--- tools/scripts/sbatch_scripts/run_sbatch_files.sh
+++ tools/scripts/sbatch_scripts/run_sbatch_files.sh
@@ -5,19 +5,25 @@ ceiled_div() {
     echo $(( (num + div - 1) / div ))
 }
 
-app=lulesh
+app=lulesh_stmp2
 cores_per_node=48
-iterations=100
+iterations=50
+max_run=1
 inputs=(40 60 80)
-task_cnt=(8 64 216 512 100)
+task_cnt=(64)
+#inputs=(40 60 80)
+#task_cnt=(8 64 216 512 100)
 
-export NUM_MPI_RANKS=0
 echo "inputs ${inputs[@]}"
 echo "task_cnt $task_cnt"
 for ntask in "${task_cnt[@]}"
 do
   node_cnt=$(ceiled_div $ntask $cores_per_node)
-  echo "sbatch --job-name=$app --nodes=$node_cnt --ntasks=$ntask --error=$app.%j.err --output=$app.%j.out --export=inputs=${inputs[@]},iterations=$iterations,NUM_MPI_RANKS=$ntask test.sbatch"
-  sbatch --job-name=$app --nodes=$node_cnt --ntasks=$ntask --error=$app.%j.err --output=$app.%j.out --export=iterations=$iterations,NUM_MPI_RANKS=$ntask scripted_run.sbatch $iterations "${inputs[@]}" 
+  export NUM_MPI_RANKS=$ntask
+  export MPICH_ASYNC_PROGRESS=1
+  export OMP_NUM_THREADS=1
+  echo "sbatch --job-name=$app --nodes=$node_cnt --ntasks=$ntask --error=$app.%j.err --output=$app.%j.out --export=iterations=$iterations,NUM_MPI_RANKS=$ntask scripted_run.sbatch $app $iterations ${inputs[*]}"
+  echo "sbatch --job-name=$app --nodes=$node_cnt --ntasks=$ntask --error=$app.%j.err --output=$app.%j.out --export=NUM_MPI_RANKS=$ntask,MPICH_ASYNC_PROGRESS=1,OMP_NUM_THREADS=1,ALL scripted_run.sbatch app:./$app iter:$iterations maxrun:$max_run inputs:${inputs[*]}"
+  sbatch --job-name=$app --nodes=$node_cnt --ntasks=$ntask --error=$app.%j.err --output=$app.%j.out --export=NUM_MPI_RANKS=$ntask,MPICH_ASYNC_PROGRESS=1,OMP_NUM_THREADS=1,ALL scripted_run.sbatch ./$app $iterations $max_run "${inputs[@]}" 
 done
 
