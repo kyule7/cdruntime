2e18821ca44331c96915742140064eec8fa2cff1
Kyushick Lee <kyushick@login2.stampede2.tacc.utexas.edu>
2018-03-30 20:52:55 -0500

    checking pipeline

--- autobuild.sh
+++ autobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p m4
 autoreconf --install
-./configure --enable-parallel=mpi --enable-logging CXX=mpicxx CXXFLAGS="-std=gnu++0x -Wall -g -O -fPIC" --enable-runtime --enable-error-injection --enable-profiler --enable-keep-preservation-files --enable-cd-test CC=icc
+./configure --enable-parallel=mpi --enable-logging CXX=mpicxx CXXFLAGS="-std=gnu++0x -Wall -g -O -fPIC" --enable-runtime --enable-error-injection --enable-profiler --enable-keep-preservation-files --enable-cd-test
 cd src/persistence/
 make -j
 cd ../../plugin/wrapper/
